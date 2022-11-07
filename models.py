aa857c57195b60c7f9efbb4a3caf490c7c9a64ab
Mauro <mau1408@outlook.com>
2022-11-07 20:06:59 -0300

    Se agrego Atributo al modelo Familia

--- models.py
+++ models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+
+class Familia(models.Model):
+    nombre = models.CharField(max_length=30)
+    edad = models.DateField(max_length=30)
+    
