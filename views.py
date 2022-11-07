f12869ec2d174449d087dda5ed9d1e7d441d7903
Mauro <mau1408@outlook.com>
2022-11-07 20:50:44 -0300

    se agregaron los Path Params

--- views.py
+++ views.py
@@ -2,4 +2,12 @@ from django.http import HttpResponse
 from MTV.models import Familia
 
 # Create your views here.
-def 
+def Mi_familia(request):
+    Familiares= Familia.object.all()
+    
+    cadena_respuesta = ""
+    for familiar in Familiares:
+        cadena_respuesta += familiar.nombre + " / "
+    
+    return HttpResponse(cadena_respuesta)
+
