f4b1013455913f3fc72195df85198d7784994a7e
Mauro <mau1408@outlook.com>
2022-11-07 20:08:34 -0300

    Se acaban de agregar otro Atributo

--- models.py
+++ models.py
@@ -5,4 +5,4 @@ from django.db import models
 class Familia(models.Model):
     nombre = models.CharField(max_length=30)
     edad = models.DateField(max_length=30)
-    
+    fecha = models.DateTimeField(max_length=30)
