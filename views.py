4ebf7b458e1bb9261829ba28a8b2d6c16ac5908b
Mauro <mau1408@outlook.com>
2022-11-07 21:19:41 -0300

    Se agregaron nuevos cambios a las views

--- views.py
+++ views.py
@@ -1,13 +1,13 @@
 from django.http import HttpResponse
 from MTV.models import Familia
+from django.template import Template, Context, loader
 
 # Create your views here.
 def Mi_familia(request):
-    Familiares= Familia.object.all()
-    
-    cadena_respuesta = ""
-    for familiar in Familiares:
-        cadena_respuesta += familiar.nombre + " / "
-    
-    return HttpResponse(cadena_respuesta)
+    Conocidos = Familia
+    datos = {"Mi familia": Familia}
+    plantilla = loader.get_template("Familiares.html")
+    documento = plantilla.render(datos)
+    return HttpResponse(documento)
+
 
